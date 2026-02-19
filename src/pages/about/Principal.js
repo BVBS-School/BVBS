393b15c9c0cc4722a4f2927e6084d6db374d00e1
BVBS-School <bvbpschool74@gmail.com>
2026-02-19 13:16:01 +0530

    Match Principal section width to Directors section

--- src/pages/about/Principal.js
+++ src/pages/about/Principal.js
@@ -7,7 +7,7 @@ import Loader from "@/Component/Loader";
 export default function Principal() {
   const [listing, setLisitng] = useState("");
   const [Loading, setLoading] = useState(false);
-  const [count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
   const principle = () => {
     setLoading(true);
     const main = new Details();
@@ -21,11 +21,10 @@ export default function Principal() {
         setLoading(false);
         setLisitng([]);
         console.log("error", err);
-        setCount(count+1);
-        if(count<=2)
-          {
-            principle();
-          }
+        setCount(count + 1);
+        if (count <= 2) {
+          principle();
+        }
       });
   };
 
@@ -34,37 +33,37 @@ export default function Principal() {
   }, []);
   return (
     <div className="bg-[#ECE1C5] py-[50px] md:py-[70px] lg:py-[100px]">
-      <div className="container sm:container md:container lg:max-w-[1200px] px-4 lg:px-0 mx-auto">
+      <div className="container sm:container md:container lg:max-w-[1204px] px-4 mx-auto">
         <h2 className="merriweather-font font-normal  text-2xl md:text-3xl lg:text-4xl mb-2 text-[#1E1E1E]  tracking-[-0.04em] mb-4 lg:mb-[37px] text-center ">
           Message from the Principal
         </h2>
         {Loading ? (
-          <Loader/>
+          <Loader />
         ) : (
-        <div className="flex flex-wrap -mx-2.5 items-center">
-          <div className="w-full md:w-6/12 px-2.5 ">
-            <div className="text-center">
-              <Image
-                blurDataURL={`${listing?.photo}?q=1`}
-                placeholder="blur"
-                className="max-w-full block mx-auto mb-3 md:mb-0"
-                alt={listing?.name}
-                src={listing?.photo}
-                height={500}
-                width={407}
-                loading="lazy"
-              />
+          <div className="flex flex-wrap -mx-2.5 items-center">
+            <div className="w-full md:w-6/12 px-2.5 ">
+              <div className="text-center">
+                <Image
+                  blurDataURL={`${listing?.photo}?q=1`}
+                  placeholder="blur"
+                  className="max-w-full block mx-auto mb-3 md:mb-0"
+                  alt={listing?.name}
+                  src={listing?.photo}
+                  height={500}
+                  width={407}
+                  loading="lazy"
+                />
+              </div>
+            </div>
+            <div className="w-full md:w-6/12 px-2.5">
+              <p className="text-[#666666] font-medium text-base lg:text-[20px] lg:leading-[30px] gotham-font mb-5 lg:mb-[28px] tracking-[-0.04em] ">
+                <q>{listing?.text}</q>
+              </p>
+              <p className="libre-baskerville font-normal text-xl md:text-[26px]  mb-0 text-[#EE834E]  tracking-[-0.04em]">
+                - {listing?.name}
+              </p>
             </div>
           </div>
-          <div className="w-full md:w-6/12 px-2.5">
-            <p className="text-[#666666] font-medium text-base lg:text-[20px] lg:leading-[30px] gotham-font mb-5 lg:mb-[28px] tracking-[-0.04em] ">
-              <q>{listing?.text}</q>
-            </p>
-            <p className="libre-baskerville font-normal text-xl md:text-[26px]  mb-0 text-[#EE834E]  tracking-[-0.04em]">
-              - {listing?.name}
-            </p>
-          </div>
-        </div>
         )}
       </div>
     </div>
